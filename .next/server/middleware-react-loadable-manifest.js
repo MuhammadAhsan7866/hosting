bbc2e3d0d3a61365a2702044165ad28ec7f42256
MuhammadAhsan7866 <solutionweb386@gmail.com>
2025-04-19 16:28:56 +0500

    api

---
+++ .next/server/middleware-react-loadable-manifest.js
@@ -0,0 +1,1 @@
+self.__REACT_LOADABLE_MANIFEST="{\"src\\\\components\\\\app\\\\LandingPage\\\\IframeResizer.jsx -> iframe-resizer/js/iframeResizer\":{\"id\":\"src\\\\components\\\\app\\\\LandingPage\\\\IframeResizer.jsx -> iframe-resizer/js/iframeResizer\",\"files\":[\"static/chunks/node_modules_iframe-resizer_js_iframeResizer_js.js\"]}}"
